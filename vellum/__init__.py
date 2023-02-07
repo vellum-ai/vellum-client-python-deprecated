ad174f7dccf80d154a9292cccaf1ef45c3ddccc9
Noa Flaherty <noaflaherty@gmail.com>
2023-02-06 20:55:05 -0800

    Update based on new response schema

--- vellum/__init__.py
+++ vellum/__init__.py
@@ -1,10 +1,12 @@
 import os
 
+from vellum.api_resources.types import GenerateRequest
 from vellum.api_resources import Generate
 
 api_key = os.environ.get("VELLUM_API_KEY")
 
 __all__ = [
     "Generate",
+    "GenerateRequest",
     "api_key",
 ]
